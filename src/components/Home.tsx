6eaf75f131bb2ec9ceb9513af1bea8d35b45d788
isamohammad <isamohammad@aineurocare.com>
2023-10-18 17:23:16 +0600

    wd

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
-    <div className="pt-20">Home</div>
+    <div className="pt-20">Home <Link to='/about' >wd</Link></div>
   )
 }
 
