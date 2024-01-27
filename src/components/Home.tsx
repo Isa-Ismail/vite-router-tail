94a36d5f6a74cf37ba2b96ac785de21a51f4e00f
isamohammad <isamohammad@aineurocare.com>
2024-01-27 15:04:31 +0600

    [NC-congrats] hello from the other side. proud of you man. keep it up

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,12 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className="pt-20">Home <Link to='/about' >wd</Link></div>
+    <div className="pt-20">
+      Home
+      <Link to='/about' >
+        wd
+      </Link>
+    </div>
   )
 }
 
